2dab4cc75d0cbca6118208b89d6aff4347ab9b89
Chris W. Burke <cburke@criticalmass.com>
2017-11-04 19:42:15 -0400

    Adding sitemap to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,9 @@ module.exports = {
   css: ['~assets/css/main.css'],
   loading: { color: '#e32559' },
   modules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-109208017-1'
-    }]
+    [
+      '@nuxtjs/google-analytics', { id: 'UA-109208017-1'},
+      '@nuxtjs/sitemap'
+    ]
  ]
 }
