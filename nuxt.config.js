6503a658f22c5b23c9bfbd3c7155ae0b37ef9128
Chris W. Burke <cburke@criticalmass.com>
2017-11-04 22:24:56 -0400

    Adding twitter card

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,13 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'We are a digital agency working with non-profits to drive social change.' }
+      { hid: 'description', name: 'description', content: 'We are a digital agency working with non-profits to drive social change.' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@onelovedigital' },
+      { name: 'twitter:title', content: 'One Love Digital' },
+      { name: 'twitter:description', content: 'We are a digital agency working with non-profits to drive social change.' },
+      { name: 'twitter:image', content: 'https://www.onelove.digital/social.jpg' },
+      { name: 'twitter:image:alt', content: 'Onelove Digital Banner' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
